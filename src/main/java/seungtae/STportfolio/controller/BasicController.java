0bb7930d871262f35cb1e838f781395a28e57a98
SeungTae <dltmdxo1589@daum.net>
2024-04-02 16:05:55 +0900

    Basic related controller

---
+++ src/main/java/seungtae/STportfolio/controller/BasicController.java
@@ -0,0 +1,32 @@
+package seungtae.STportfolio.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@Slf4j
+public class BasicController {
+
+    @GetMapping("portfolio")
+    public String portfolio() {
+        return "page/portfolio";
+    }
+
+    @GetMapping("contact")
+    public String contact() {
+        return "page/contact";
+    }
+
+    @GetMapping("login")
+    public String hideLogin() {
+        return "page/login";
+    }
+
+    @GetMapping("manage")
+    public String hideManage() {
+        return "page/management";
+    }
+}
