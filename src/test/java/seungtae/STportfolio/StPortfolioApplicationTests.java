89e34407296c3a7aaf55f639f25bed04f746ff86
SeungTae <dltmdxo1589@daum.net>
2024-04-02 15:35:37 +0900

    First commit

---
+++ src/test/java/seungtae/STportfolio/StPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package seungtae.STportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
