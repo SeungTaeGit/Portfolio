fdf2358f903e6db71a9e6e3b99d658dc8339e1ed
SeungTae <dltmdxo1589@daum.net>
2024-04-04 23:12:29 +0900

    Add Login related Service

---
+++ src/main/java/seungtae/STportfolio/service/LoginService.java
@@ -0,0 +1,22 @@
+package seungtae.STportfolio.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import seungtae.STportfolio.dto.Manager;
+import seungtae.STportfolio.repository.MemberRepository;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    // 추후 개발 진행
+    public Manager login(String Id, String pw) {
+        return memberRepository.findById(Id)
+                .filter(m -> m.getPassword().equals(pw))
+                .orElse(null);
+    }
+}
