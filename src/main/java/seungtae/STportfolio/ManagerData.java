3165be3fb33935f799b88e53b000857cac644b5f
SeungTae <dltmdxo1589@daum.net>
2024-04-04 23:13:45 +0900

    Add Test Manager Data

---
+++ src/main/java/seungtae/STportfolio/ManagerData.java
@@ -0,0 +1,22 @@
+package seungtae.STportfolio;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import seungtae.STportfolio.dto.Manager;
+import seungtae.STportfolio.repository.MemberRepository;
+
+@Component
+@RequiredArgsConstructor
+public class ManagerData {
+
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init() {
+        Manager manager = new Manager();
+        manager.setId("SeungTae");
+        manager.setPassword("pw");
+        memberRepository.save(manager);
+    }
+}
