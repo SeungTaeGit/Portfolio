8c95ca8f9c304649df4733a7ee6a3018dafdaf9c
SeungTae <dltmdxo1589@daum.net>
2024-04-04 23:13:10 +0900

    Add Member related Repository

---
+++ src/main/java/seungtae/STportfolio/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package seungtae.STportfolio.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import seungtae.STportfolio.dto.Manager;
+
+import java.util.*;
+
+@Repository
+@Slf4j
+public class MemberRepository {
+
+    private static Map<Long, Manager> store = new HashMap<>();
+
+    public Manager save(Manager manager) {
+        store.put(manager.getSequence(), manager);
+        return manager;
+    }
+
+    public Manager findBySequence(Long sequence) {
+        return store.get(sequence);
+    }
+
+    public Optional<Manager> findById(String Id) {
+        return findAll().stream()
+                .filter(m -> m.getId().equals(Id))
+                .findFirst();
+    }
+
+    public List<Manager> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
