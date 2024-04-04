88018c3eae982dc434aec3ac0aa7cff32cb8126f
SeungTae <dltmdxo1589@daum.net>
2024-04-04 23:14:04 +0900

    Add Data Transfer Object

---
+++ src/main/java/seungtae/STportfolio/dto/Manager.java
@@ -0,0 +1,14 @@
+package seungtae.STportfolio.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Manager {
+
+    private Long sequence = 1L;
+
+    private String id;
+
+    private String password;
+}
---
+++ src/main/java/seungtae/STportfolio/dto/Portfolio.java
@@ -0,0 +1,13 @@
+package seungtae.STportfolio.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter @Setter
+public class Portfolio {
+
+    private String title;
+    private String date;
+}
